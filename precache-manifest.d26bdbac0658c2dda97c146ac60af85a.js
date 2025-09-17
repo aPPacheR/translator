a5482c89ee812c24067d3c892306045767e65645
Pavlenko Pavel <87997793+aPPacheR@users.noreply.github.com>
2025-09-17 22:29:02 +0300

    Add files via upload

---
+++ precache-manifest.d26bdbac0658c2dda97c146ac60af85a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7466aaa4d57b56f2efcbf324e1ea58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f328a253e66def1abe0a",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "f328a253e66def1abe0a",
+    "url": "/static/js/main.91cba783.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
